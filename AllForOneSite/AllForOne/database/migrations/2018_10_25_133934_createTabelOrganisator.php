e2a61e5ec2275399918973efddbb814b68271459
vincent-wathelet <32623140+vincent-wathelet@users.noreply.github.com>
2018-11-05 20:34:06 +0100

    Vincent dev (#3)
    * ERD begin mockups
    
    * opstart en migrations
    
    opstart project maken van database migrations en login geactiveerd

---
+++ AllForOneSite/AllForOne/database/migrations/2018_10_25_133934_createTabelOrganisator.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTabelOrganisator extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organisatoren', function (Blueprint $table) {
+            $table->integer('eventId',false,true);
+            $table->integer('userId',false,true);
+            $table->string('titel');
+            $table->foreign('eventId')->references('id')->on('event');
+            $table->foreign('userId')->references('id')->on('users');
+            $table->timestamps();
+            $table->primary(array('eventId','userId'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organisatoren');
+    }
+}
