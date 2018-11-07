59b44fc4044cd62b0fb8d99042b7605dd19583e9
vincent-wathelet <32623140+vincent-wathelet@users.noreply.github.com>
2018-11-07 20:18:39 +0100

    master to adbelai-dev branch (#5)
    * ERD begin mockups (#2)
    
    * Vincent dev (#3)
    
    * ERD begin mockups
    
    * opstart en migrations
    
    opstart project maken van database migrations en login geactiveerd

---
+++ AllForOneSite/AllForOne/database/migrations/2018_10_25_095513_createEventTable.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event', function (Blueprint $table) {
+
+            $table->integer('id',true,true);
+            $table->integer('categorieId',false,true);
+            $table->integer('name');
+            $table->string('naam');
+            $table->integer('lokaalId',false,true);
+            $table->integer('maxInschrijvingen');
+            $table->datetime('date');
+            $table->time('duur');
+            $table->boolean('autoaccept');
+            $table->string('description');
+            $table->boolean('hidden');
+            $table->unique(['id','categorieId']);
+            $table->timestamps();
+            $table->foreign('categorieId')->references('id')->on('categorie');
+            $table->foreign('lokaalId')->references('id')->on('lokaal');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+//        Schema::table('event', function (Blueprint $table) {
+//            $table->dropForeign('posts_user_id_foreign');
+//        });
+        Schema::dropIfExists('event');
+    }
+}
