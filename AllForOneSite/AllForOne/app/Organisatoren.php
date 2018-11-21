9514fe62766b229f5342b37f7b88307a5d3090f0
vincent-wathelet <32623140+vincent-wathelet@users.noreply.github.com>
2018-11-21 17:10:22 +0100

    pushen main naar ali (#10)
    * ERD begin mockups
    
    * opstart en migrations
    
    opstart project maken van database migrations en login geactiveerd
    
    * Abdelali dev (#7)
    
    * test
    
    test
    
    * master to adbelai-dev branch (#5)
    
    * ERD begin mockups (#2)
    
    * Vincent dev (#3)
    
    * ERD begin mockups
    
    * opstart en migrations
    
    opstart project maken van database migrations en login geactiveerd
    
    * Views & Controllers
    
    Views en controllers van AllEvents, Homepage & MyEventsEntries
    
    * Vincent dev (#9)
    
    * pull master to branch (#8)
    
    * test (#1)
    
    test
    
    * ERD begin mockups (#2)
    
    * Vincent dev (#3)
    
    * ERD begin mockups
    
    * opstart en migrations
    
    opstart project maken van database migrations en login geactiveerd
    
    * Mohamed dev (#6)
    
    * test
    
    test
    
    * Vincent dev (#3) (#4)
    
    * ERD begin mockups
    
    * opstart en migrations
    
    opstart project maken van database migrations en login geactiveerd
    
    * 3 webpagina's
    
    controllers, routes en views aangemaakt voor My Events, My Entries en My Event Details.
    
    * Abdelali dev (#7)
    
    * test
    
    test
    
    * master to adbelai-dev branch (#5)
    
    * ERD begin mockups (#2)
    
    * Vincent dev (#3)
    
    * ERD begin mockups
    
    * opstart en migrations
    
    opstart project maken van database migrations en login geactiveerd
    
    * Views & Controllers
    
    Views en controllers van AllEvents, Homepage & MyEventsEntries
    
    * toeveogen model
    
    alle models en connectie met Eloquent toegevoegd
    
    * aanmaken migrations
    
    update migrations en aanmaken log tabel
    
    * vieuws
    
    maken vieuws contact landingpage layout page en toeveogen aan routes

---
+++ AllForOneSite/AllForOne/app/Organisatoren.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property Event $event
+ * @property User $user
+ * @property int $eventId
+ * @property int $userId
+ * @property string $titel
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Organisatoren extends Model
+{
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'organisatoren';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['titel', 'created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function event()
+    {
+        return $this->belongsTo('App\Event', 'eventId');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'userId');
+    }
+}
---
+++ AllForOneSite/AllForOne/database/migrations/2018_11_19_065927_create_inschrijvings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInschrijvingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inschrijvings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('eventid',false,true);
+            $table->integer('userid',false,true);
+            $table->boolean('bevestigt');
+            $table->foreign('eventid')->references('id')->on('event');
+            $table->foreign('userid')->references('id')->on('users');
+            $table->unique(['eventid','userid']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inschrijvings');
+    }
+}
